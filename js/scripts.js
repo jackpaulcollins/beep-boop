3175c4c6ba1860e0702d6c91a2d0d14d4ee53d32
Jack <jackpaulcollins@gmail.com>
2019-01-25 15:48:28 -0800

    I'm not sure

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ function numTranslate(userNum){
   name = $("#name").val();
 
   for (var i = 1; i <= userNum; i++) {
-    if (i.toString().includes("3")) {
+
+    if (i<10 && i%5 === 0) {
+      outputList.push(" See, I am smart. This number is five ")
+    }
+    else if (i.toString().includes("3")) {
       outputList.push(" I'm sorry, " + name + ". I'm afraid I can't do that. " );
     } else if (i.toString().includes("2")) {
       outputList.push(" Boop! ");
